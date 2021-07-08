beeacfb648dac6c1aafad6e18e738d382fe60ade
ianwhitehill <smwirbw@gmail.com>
2021-07-08 13:35:01 -0500

    put it together

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
 class User extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
     render(){
         return(
             <div>
                 <h1>{this.props.firstName}, {this.props.lastName}</h1>
-                <h3>Age: {this.props.age}</h3>
+                <h3>Age: {this.state.age}</h3>
                 <h3>Hair Color: {this.props.hairColor}</h3>
+                <button onClick= {() => {this.setState({age: this.state.age + 1})}}>Birthday</button>
             </div>
         );
     }
