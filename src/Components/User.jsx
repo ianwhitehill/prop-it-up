4f63e8d3b273d7097c0d0a9bc4b812efe74d4caf
ianwhitehill <smwirbw@gmail.com>
2021-07-08 12:51:05 -0500

    first commit

---
+++ src/Components/User.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class User extends Component{
+    render(){
+        return(
+            <div>
+                <h1>{this.props.firstName}, {this.props.lastName}</h1>
+                <h3>Age: {this.props.age}</h3>
+                <h3>Hair Color: {this.props.hairColor}</h3>
+            </div>
+        );
+    }
+}
+
+export default User;
